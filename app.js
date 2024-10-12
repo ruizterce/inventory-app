839c704a36b3022f83f90ad30ab36f3d2e4c2ef7
ruizterce <127963868+ruizterce@users.noreply.github.com>
2024-10-12 13:18:19 +0200

    Setup initial express MVC structure

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const PORT = 3000;
+const indexRouter = require("./routes/indexRouter");
+const assetsPath = path.join(__dirname, "public");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", indexRouter);
+
+app.listen(PORT, () => console.log(`Express app - listening on port ${PORT}`));
