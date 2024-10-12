867ef9c54b6a7e91416bde22309ab7685e2b2f2f
ruizterce <127963868+ruizterce@users.noreply.github.com>
2024-10-12 17:26:53 +0200

    Implement getProductsByCategoryId

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,16 @@
+const db = require("../db/queries");
+
 module.exports = {
-  get: (req, res) => {
-    res.render("category", { categoryId: req.params.categoryId });
+  get: async function (req, res) {
+    try {
+      const products = await db.getProductsByCategoryId(req.params.categoryId);
+      res.render("category", {
+        categoryId: req.params.categoryId,
+        products: products,
+      });
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      res.status(500).send("Internal Server Error");
+    }
   },
 };
--- db/queries.js
+++ db/queries.js
@@ -1,15 +1,25 @@
 const pool = require("./pool");
 
-async function getAllProducts() {
-  try {
-    const { rows } = await pool.query("SELECT * FROM products");
-    return rows;
-  } catch (error) {
-    console.error("Error retrieving products:", error);
-    throw new Error("Could not retrieve products. Please try again later.");
-  }
-}
-
 module.exports = {
-  getAllProducts,
+  getAllProducts: async () => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM products");
+      return rows;
+    } catch (error) {
+      console.error("Error retrieving products:", error);
+      throw new Error("Could not retrieve products. Please try again later.");
+    }
+  },
+
+  getProductsByCategoryId: async (categoryId) => {
+    try {
+      const { rows } = await pool.query(
+        `SELECT * FROM Products p WHERE category_id = ${categoryId}`
+      );
+      return rows;
+    } catch (error) {
+      console.error("Error retrieving products:", error);
+      throw new Error("Could not retrieve products. Please try again later.");
+    }
+  },
 };
