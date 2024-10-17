babf24e42840f3c26c672e6381b41f3a5708daa7
ruizterce <127963868+ruizterce@users.noreply.github.com>
2024-10-17 12:43:46 +0200

    Integrate Tailwind CSS and layouts, refactor views
    - Installed Tailwind CSS for utility-first styling
      - Added `tailwind.config.js` and
      `public/css/tailwind.css`
      - Updated `.gitignore`
    - Integrated `express-ejs-layouts` to manage
    layouts across views
      - Added `layout.ejs` for common page structure
      - Created partials (`header.ejs`, `footer.ejs`)
      - Refactored all views to use layout
    - Updated `app.js` to include
    `express-ejs-layouts` and Tailwind setup

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const expressLayouts = require("express-ejs-layouts");
 const path = require("node:path");
 const PORT = 3000;
 const indexRouter = require("./routes/indexRouter");
@@ -7,6 +8,9 @@ const assetsPath = path.join(__dirname, "public");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(expressLayouts);
+app.set("layout", "layout");
 
 app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
